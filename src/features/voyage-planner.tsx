d66633465d3fda6392463aa1f722a51b88a18679
Alejandro Garcia <92.alejandrogarcia@gmail.com>
2022-07-27 22:56:40 +0200

    fetch ports data; populate select with ports data

--- src/features/voyage-planner.tsx
+++ src/features/voyage-planner.tsx
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import RouteMap from "./route-map"
 import "./voyage-planner.css"
+import { useAppSelector, useAppDispatch } from '../services/hooks'
+import { fetchPorts } from '../services/ports-reducer';
 
 interface VoyagePlannerParams{
 
 }
 
+
 function VoyagePlanner(params: VoyagePlannerParams){
+    const dispatch = useAppDispatch()
+    const state = useAppSelector(state => state.ports)
+
+    useEffect(() => {
+        async function fetchPortsData() {
+            if (state.ports.length <= state.count) {
+                const response = fetchPorts(state.offset)
+                const data = JSON.parse(response)
+                data && dispatch({ type: 'ADD_PORTS', payload: data })
+            }
+        }
+        fetchPortsData()
+    }, [state, dispatch])
 
     return (
         <div className="voyage-planner-container">
             <div className="voyage-route-container">
                 <h2>Route Planner</h2>
                 <div className="port-locator">
-                    <select id="port-select"/> <button>+</button>
+                    <select id="port-select">
+                    {
+                        state.ports.map((port) => (
+                            <option value={port.uncode}>{port.name}</option>
+                        ))
+                    }
+                    </select>
+                    <button>+</button>
                 </div>
                 <div className="voyage-listing">
                     <h2>Voyage</h2>
